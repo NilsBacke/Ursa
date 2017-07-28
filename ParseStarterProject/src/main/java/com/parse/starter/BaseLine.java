785c57532ca3dca45951a0639eb46f83fd9469d6
Noah <noahwoodward2244@gmail.com>
2017-07-27 21:25:56 -0400

    drawable add2

---
+++ ParseStarterProject/src/main/java/com/parse/starter/BaseLine.java
@@ -0,0 +1,44 @@
+package com.parse.starter;
+
+/**
+ * Created by nWoody on 7/27/2017.
+ */
+
+public class BaseLine {
+    public BaseLine() {
+    }
+
+   public double  getBmiScore( double bmi) {
+       if(bmi>=18.5 || bmi <=24.9){
+           return .01 * bmi;
+       }
+       else {
+           return .02 *bmi;
+       }
+    }
+    public double getAgeScore(int age){
+       if(age>=20 && age<=35) {
+           return .005 * age;
+       }
+       else if (age < 20 || (age>40 && age<=51)){
+           return .01 *  age;
+       }
+       else if(age>35 && age<=40){
+
+           return .008 * age;
+       }
+       else if (age>51){
+
+           return .006*age;
+       }
+       else{
+           return -1;
+       }
+
+    }
+    public double getBaseLineScore(int age,double bmi){
+        return getAgeScore(age) + getBmiScore(bmi);
+    }
+
+
+}
